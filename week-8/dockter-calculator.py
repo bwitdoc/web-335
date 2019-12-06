5ab03fb02f77056b91a068071396af440429fa6d
Brittany Dockter <bwitdoc@gmail.com>
2019-12-05 16:53:38 -0800

    adding week 8 assignments

---
+++ week-8/dockter-calculator.py
@@ -0,0 +1,16 @@
+num1 = 9
+num2 = 3
+
+def add(a, b):
+    return(a+b)
+
+def sub(a, b):
+    return(a-b)
+
+def div(a, b):
+    return (a/b)
+
+
+print(add(num1, num2))
+print(sub(num1, num2))
+print(div(num1, num2))
